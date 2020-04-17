ca41c99304d63400bb7ddb11920d8bdde371388c
iproman <itechman@yandex.ru>
2020-04-17 16:54:37 +0500

    ComboBoxes with Examples.

---
+++ 7 - ComboBoxes with Examples/tutorial7.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(869, 601)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.comboX = QtWidgets.QComboBox(self.centralwidget)
+        self.comboX.setGeometry(QtCore.QRect(50, 120, 231, 121))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.comboX.setFont(font)
+        self.comboX.setObjectName("comboX")
+        self.comboX.addItem("")
+        self.comboX.addItem("")
+        self.comboY = QtWidgets.QComboBox(self.centralwidget)
+        self.comboY.setGeometry(QtCore.QRect(470, 120, 231, 121))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.comboY.setFont(font)
+        self.comboY.setObjectName("comboY")
+        self.comboY.addItem("")
+        self.comboY.addItem("")
+        self.submit = QtWidgets.QPushButton(self.centralwidget)
+        self.submit.setGeometry(QtCore.QRect(290, 420, 221, 91))
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        self.submit.setFont(font)
+        self.submit.setObjectName("submit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(310, 300, 221, 81))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 869, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.submit.clicked.connect(self.pressed)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.comboX.setItemText(0, _translate("MainWindow", "0"))
+        self.comboX.setItemText(1, _translate("MainWindow", "1"))
+        self.comboY.setItemText(0, _translate("MainWindow", "0"))
+        self.comboY.setItemText(1, _translate("MainWindow", "1"))
+        self.submit.setText(_translate("MainWindow", "Submit"))
+        self.label.setText(_translate("MainWindow", "X XOR Y ="))
+
+    def pressed(self):
+        x = int(self.comboX.currentText())
+        y = int(self.comboY.currentText())
+        xor = (x and not y) or (not x and y)
+        if xor == True:
+            xor = 1
+        else:
+            xor = 0
+
+        self.label.setText("X XOR Y = " + str(xor))
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
